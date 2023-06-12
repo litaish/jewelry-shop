ed574ec4d5ce87d6ff7b1dd1c2ecc2ea12032e56
Lita Strode <l.strode000@gmail.com>
2023-06-12 18:15:45 +0300

    Style Home view

---
+++ src/pages/Home/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Hero.module.css';
+import { Link } from 'react-router-dom';
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+    <h3 className={styles.title}>Discover Your Perfect Sparkle</h3>
+    <p className={styles.paragraph}>Step into a world of shimmering enchantment at GemLuxe today! 
+        Explore our curated collection of exquisite jewelry, meticulously crafted to ignite your inner 
+        radiance.
+        </p>
+       <Link to='/shop' className={styles.button}>View more
+       </Link>
+  </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home/HomeView.jsx
+++ src/pages/Home/HomeView.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from './HomeView.module.css';
+import Hero from './Hero';
 
 const HomeView = () => {
-  return <div>homeview</div>;
+  return (
+    <div className={styles.background}>
+    <Hero />
+  </div>
+  );
 };
 
 export default HomeView;
